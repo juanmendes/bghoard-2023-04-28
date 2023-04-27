c3ea35ece0e7618d166b68aae766e6851d4523d3
Juan Mendes <mendejuan@gmail.com>
2023-04-27 14:10:02 -0400

    Lab 10 - Add Material to storybook

--- libs/store/ui-shared/src/lib/header/header.component.stories.ts
+++ libs/store/ui-shared/src/lib/header/header.component.stories.ts
@@ -1,9 +1,15 @@
-import { Meta } from '@storybook/angular';
+import { Meta, moduleMetadata } from '@storybook/angular';
 import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 export default {
   title: 'HeaderComponent',
   component: HeaderComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatToolbarModule], // <-- import the module
+    }),
+  ],
 } as Meta<HeaderComponent>;
 
 export const Primary = {
