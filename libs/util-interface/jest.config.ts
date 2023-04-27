68fe45537f7d30592914e0a5e995aaf844df5225
Juan Mendes <mendejuan@gmail.com>
2023-04-27 13:57:18 -0400

    Lab 9 - Move api-util-interface to util-interface
    Moves lib from /libs/api/util-interfaces to
    /libs/util-interfaces

--- libs/util-interface/jest.config.ts
+++ libs/util-interface/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'api-util-interface',
-  preset: '../../../jest.preset.js',
+  displayName: 'util-interface',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/api/util-interface',
+  coverageDirectory: '../../coverage/libs/util-interface',
 };
