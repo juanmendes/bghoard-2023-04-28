3e7ceb2822150687197c3cc8af8700249463d935
Juan Mendes <mendejuan@gmail.com>
2023-04-27 11:53:30 -0400

    Lab 5 - Use formatRating in store app

--- libs/store/util-formatters/src/lib/store-util-formatters.ts
+++ libs/store/util-formatters/src/lib/store-util-formatters.ts
@@ -1,3 +1,3 @@
-export function storeUtilFormatters(): string {
-  return 'store-util-formatters';
+export function formatRating(rating: number) {
+  return `${Math.round(rating * 100) / 10} / 10`;
 }
